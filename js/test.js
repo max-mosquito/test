9e362e2783e857e3556bb35db5b86a9e3700d0fb
Max <asdsad.@sae.com>
2021-02-09 23:19:25 +0300

    4commit

--- js/test.js
+++ js/test.js
@@ -22,23 +22,27 @@
 Проверить, чтобы все работало без ошибок в консоли */
 
 "use strict";
-let numberOfFilms = +prompt('Cколько фильмов Вы уже посмтрели?', '');
-let personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
+// let numberOfFilms = +prompt('Cколько фильмов Вы уже посмтрели?', '');
+// let personalMovieDB = {
+// 	count: numberOfFilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false
+// };
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
-let a = prompt('Один из последних просмотренных фильмов?', ""),
-	b = prompt('На сколько оцените его', ""),
-	c = prompt('Один из последних просмотренных фильмов?', ""),
-	d = prompt('На сколько оцените его?', "");
+// let a = prompt('Один из последних просмотренных фильмов?', ""),
+// 	b = prompt('На сколько оцените его', ""),
+// 	c = prompt('Один из последних просмотренных фильмов?', ""),
+// 	d = prompt('На сколько оцените его?', "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+//----------------------------------------
+let a = 4;
+a++;
+console.log(a);
